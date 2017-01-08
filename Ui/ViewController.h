b8978fbe538121cd3659841d06f4de3afaad818e
王鹏 <wangpeng@kankan.local>
2017-01-08 10:24:11 +0800

    Initial Commit

---
+++ Ui/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ui
+//
+//  Created by 王鹏 on 2017/1/8.
+//  Copyright © 2017年 王鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Ui/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Ui
+//
+//  Created by 王鹏 on 2017/1/8.
+//  Copyright © 2017年 王鹏. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
